b5648c0decd5c1cdbec65d87eca842d5c6f8c0fa
Kaviraj <ksb1910.07@gmail.com>
2022-01-29 09:22:28 +0530

    given sum efficient approach

--- src/com/subarrayquestions/SubarrayGivenSum.java
+++ src/com/subarrayquestions/SubarrayGivenSum.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class SubarrayGivenSum {
     public static void main(String[] args) {
         int[] arr = {5, 8, 6, 13, 3, -1};
-        System.out.println(givenSumNaive(arr, 22));
+        System.out.println(givenSumEfficient(arr, 22));
     }
 
     static boolean givenSumNaive(int[] arr, int sum) {
@@ -16,6 +16,20 @@ public class SubarrayGivenSum {
                 if (currSum == sum)
                     return true;
             }
+        }
+        return false;
+    }
+
+    static boolean givenSumEfficient(int[] arr, int sum) {
+        HashSet<Integer> set = new HashSet<>();
+        int preSum = 0;
+        for (int x : arr) {
+            preSum += x;
+            if (set.contains(preSum - sum))
+                return true;
+            if (preSum == sum)
+                return true;
+            set.add(preSum);
         }
         return false;
     }
