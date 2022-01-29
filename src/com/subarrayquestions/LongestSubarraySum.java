b6064cfc34a620ce2d64fd530a43f284763aac64
Kaviraj <ksb1910.07@gmail.com>
2022-01-29 21:09:57 +0530

    longest subarray sum efficient approach

--- src/com/subarrayquestions/LongestSubarraySum.java
+++ src/com/subarrayquestions/LongestSubarraySum.java
@@ -1,9 +1,11 @@
 package com.subarrayquestions;
 
+import java.util.HashMap;
+
 public class LongestSubarraySum {
     public static void main(String[] args) {
         int[] arr = {3, 1, 0, 1, 8, 2, 3, 6};
-        System.out.println(longestSubarrayNaive(arr, 5));
+        System.out.println(longestSubarrayEfficient(arr, 5));
     }
 
     static int longestSubarrayNaive(int[] arr, int sum) {
@@ -17,5 +19,20 @@ public class LongestSubarraySum {
             }
         }
         return maxLength;
+    }
+
+    static int longestSubarrayEfficient(int[] arr, int sum) {
+        int preSum = 0, res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            preSum += arr[i];
+            if (preSum == sum)
+                res = i + 1;
+            if (!map.containsKey(preSum))
+                map.put(preSum, i);
+            if (map.containsKey(preSum - sum))
+                res = Math.max(res, i - map.get(preSum - sum));
+        }
+        return res;
     }
 }
